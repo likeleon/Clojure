24a3a54b15967a97c9adb6324cb94f89e743c63f
Sung Ho Kim <likeleon@gmail.com>
2016-10-02 19:59:32 +0900

    cftbat 06

---
+++ cftbat/src/cftbat/06/core.clj
@@ -0,0 +1,104 @@
+(ns cftbat.06.core
+  (:require [clojure.string :as s]
+            [clojure.java.browse :as browse])
+  (:refer-clojure :exclude [min max]))
+
+(defn comparator-over-maps
+  [comparison-fn ks]
+  (fn [maps]
+    (zipmap ks
+            (map #(apply comparison-fn (map % maps))
+                 ks))))
+
+(def min (comparator-over-maps clojure.core/min [:lat :lng]))
+(def max (comparator-over-maps clojure.core/max [:lat :lng]))
+
+(defn translate-to-00
+  [locations]
+  (let [mincoords (min locations)]
+    (map #(merge-with - % mincoords) locations)))
+
+(defn scale
+  [width height locations]
+  (let [maxcoords (max locations)
+        ratio {:lat (/ height (:lat maxcoords))
+               :lng (/ width (:lng maxcoords))}]
+    (map #(merge-with * % ratio) locations)))
+
+(defn latlng->point
+  "Convert lat/lng map to comma-separated string"
+  [latlng]
+  (str (:lat latlng) "," (:lng latlng)))
+
+(defn points
+  "Given a seq of lat/lng maps, return string of points joined by space"
+  [locations]
+  (s/join " " (map latlng->point locations)))
+
+(defn line
+  [points]
+  (str "<polyline points=\"" points "\" />"))
+
+(defn transform
+  "Just chains other functions"
+  [width height locations]
+  (->> locations
+       translate-to-00
+       (scale width height)))
+
+(defn xml
+  "svg 'template', which also flips the coordinate system"
+  [width height locations]
+  (str "<svg height=\"" height "\" width=\"" width "\">"
+       ;; These two <g> tags change the coordinate system so that
+       ;; 0,0 is in the lower-left corner, instead of SVG's default
+       ;; upper-left corner
+       "<g transform=\"translate(0," height ")\">"
+       "<g transform=\"rotate(-90)\">"
+       (-> (transform width height locations)
+           points
+           line)
+       "</g></g>"
+       "</svg>"))
+
+(def heists [{:location "Cologne, Germany"
+              :cheese-name "Archbishop Hildebold's Cheese Pretzel"
+              :lat 50.95
+              :lng 6.97}
+             {:location "Zurich, Switzerland"
+              :cheese-name "The Standard Emmental"
+              :lat 47.37
+              :lng 8.55}
+             {:location "Marseille, France"
+              :cheese-name "Le Fromage de Cosquer"
+              :lat 43.30
+              :lng 5.37}
+             {:location "Zurich, Switzerland"
+              :cheese-name "The Lesser Emmental"
+              :lat 47.37
+              :lng 8.55}
+             {:location "Vatican City"
+              :cheese-name "The Cheese of Turin"
+              :lat 41.90
+              :lng 12.45}])
+
+(defn url
+  [filename]
+  (str "file:///"
+       (System/getProperty "user.dir")
+       "/"
+       filename))
+
+(defn template
+  [contents]
+  (str "<style>polyline { fill:none; stroke:#5881d8; stroke-width:3}</style>"
+       contents))
+
+(defn -main
+  [& args]
+  (let [filename "map.html"]
+    (->> heists
+         (xml 50 100)
+         template
+         (spit filename))
+    (browse/browse-url (url filename))))
