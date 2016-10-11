c1603c5f15e6a0510b62be32731be8cf079ce929
Sung Ho Kim <likeleon@gmail.com>
2016-10-11 22:24:16 +0900

    cftbat 9장 연습문제 2

--- cftbat/src/cftbat/10/core.clj
+++ cftbat/src/cftbat/10/core.clj
@@ -6,3 +6,16 @@
 (swap! counter inc)
 (swap! counter inc)
 (println @counter)
+
+;; exercise 2
+(defn quote-word-count-single
+  []
+  (frequencies (re-seq #"\w+" (slurp "http://www.braveclojure.com/random-quote"))))
+
+(defn quote-word-count
+  [quote-count]
+  (let [counts (atom {})
+        counts-per-quote (doall (repeatedly quote-count #(future (quote-word-count-single))))]
+    (doseq [count-per-quote counts-per-quote]
+      (swap! counts #(merge-with + % @count-per-quote)))
+    @counts))
